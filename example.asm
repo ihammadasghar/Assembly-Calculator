0b76b354f9cb3385e476bf324ec23face120c0b7
Hammad Asghar <ihammadasghar@gmail.com>
2022-05-30 17:30:50 +0100

    Cos function finished

--- example.asm
+++ example.asm
@@ -13,18 +13,6 @@ syscall
 move $t0,$v0 #to contains the input of the user
 
 
-# Take numbers as input
-# save num A
-li $v0,5
-syscall 
-move $t1,$v0
-
-# save num B
-li $v0,5
-syscall 
-move $t2,$v0
-
-
 # choosing operation number
 beq $t0,1,soma
 beq $t0,2, subt
@@ -37,22 +25,77 @@ beq $t0,7, cos
 
 # Operacoes
 soma:
-add $a0,$t1,$t2  # add location, num1, num2
-b print
+    # Take numbers as input
+    # save num A
+    li $v0,5
+    syscall 
+    move $t1,$v0
+
+    # save num B
+    li $v0,5
+    syscall 
+    move $t2,$v0
+
+    add $a0,$t1,$t2  # add location, num1, num2
+    b print
 
 subt:
-sub $a0,$t1,$t2
-b print
+    # Take numbers as input
+    # save num A
+    li $v0,5
+    syscall 
+    move $t1,$v0
+
+    # save num B
+    li $v0,5
+    syscall 
+    move $t2,$v0
+
+    sub $a0,$t1,$t2
+    b print
 
 divide:
-div $a0,$t1,$t2
-b print
+    # Take numbers as input
+    # save num A
+    li $v0,5
+    syscall 
+    move $t1,$v0
+
+    # save num B
+    li $v0,5
+    syscall 
+    move $t2,$v0
+
+    div $a0,$t1,$t2
+    b print
 
 multi:
-mul $a0,$t1,$t2
-b print
+    # Take numbers as input
+    # save num A
+    li $v0,5
+    syscall 
+    move $t1,$v0
+
+    # save num B
+    li $v0,5
+    syscall 
+    move $t2,$v0
+
+    mul $a0,$t1,$t2
+    b print
 
 exponent:
+    # Take numbers as input
+    # save num A
+    li $v0,5
+    syscall 
+    move $t1,$v0
+
+    # save num B
+    li $v0,5
+    syscall 
+    move $t2,$v0
+
     # t1 - base
     # t2 - power
     # t3 - loop index
@@ -67,6 +110,17 @@ exponent:
     b print
 
 root:
+    # Take numbers as input
+    # save num A
+    li $v0,5
+    syscall 
+    move $t1,$v0
+
+    # save num B
+    li $v0,5
+    syscall 
+    move $t2,$v0
+
     # t1 - base
     # t2 - root power
     # t3 - index
@@ -91,44 +145,59 @@ root:
         b rootloop
 
 cos:
-    # t1 - x
-    # t5 - x raised to some power
-    # t6 - approximation level
+    # Take input x
+    li $v0,6
+    syscall 
 
-    li $t6,1000
+    # t1 - x
+    # t6 - approximation level
+    li $t6,10
     
     # HAVE TO CONVERT INT 1 TO FLOAT BECAUSE LI.S DOESNT WORK WITH MARS
     li $t4, 1
     mtc1 $t4, $f2
-    cvt.s.w $f2, $f2
-    
+    cvt.s.w $f2, $f2 # final result variable
 
     li $t3,2 # loop index
+    li $t0,1 # 1 - subtract series, 0 - add series
     cosloop:
-        li $t4,0 # inner loop index
-        li $t5,1 # result variable for exponenet
+        # HAVE TO CONVERT INT 1 TO FLOAT BECAUSE LI.S DOESNT WORK WITH MARS
+        li $t4, 1
+        mtc1 $t4, $f1
+        cvt.s.w $f1, $f1
+        
+        li $t4, 0 # inner index
         cosloop2: # Multiply index by itself "root power" times
-            mul $t5,$t5,$t1
+            mul.s $f1,$f1,$f0
             add $t4,$t4,1 # increment inner loop index
             blt $t4,$t3,cosloop2
+
 
         move $t4,$t3 # inner loop index
         li $t7,1 # result variable for factorial
         cosloop3: # get factorial of index
-            mul $t5,$t5,$t4
-            sub $t4,$t4,1 # descrement the inner index
+            mul $t7,$t7,$t4
+            sub $t4,$t4,1 # decrement the inner index
             bgt $t4,1,cosloop3
-        
-        # Convert to float
-        mtc1 $t5, $f6
-        cvt.s.w $f6, $f6
 
-        # Convert to float
+        # Convert t7(factorial result) to float
         mtc1 $t7, $f8
         cvt.s.w $f8, $f8
 
-        div.s $f4,$f6,$f8 # divide exponenet result by factorial result
-        sub.s $f2,$f2,$f4 # subtract from the cos final result
+        div.s $f4,$f1,$f8 # divide exponenet result by factorial result
+
+        # if t8 is 1 subtract from result
+        beq $t0,1,subtractFromRes
+        # else
+            add.s $f2,$f2,$f4
+            li $t0,1
+            b continue
+
+        subtractFromRes:
+            sub.s $f2,$f2,$f4 # subtract from the cos final result
+            li $t0,0 
+
+        continue:
         add $t3,$t3,2 # increment the index
         blt $t3,$t6,cosloop # check if the loop should end
     mov.s $f12,$f2
@@ -138,6 +207,7 @@ cos:
 print: 
 li $v0,1
 syscall
+
 
 printfloat:
 li $v0,2
